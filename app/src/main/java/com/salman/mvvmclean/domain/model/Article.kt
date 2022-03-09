12f53e68d2bf2225238fe872d6122bcab9cedb07
7peaks <admin@Admins-Mac-mini.local>
2022-03-09 14:41:12 +0700

    update Article Model class

--- app/src/main/java/com/salman/mvvmclean/domain/model/Article.kt
+++ app/src/main/java/com/salman/mvvmclean/domain/model/Article.kt
@@ -1,12 +1,6 @@
 package com.salman.mvvmclean.domain.model
 
 data class Article(
-    val id: Int,
     val imageUrl: String,
-    val newsSite: String,
-    val publishedAt: String,
-    val summary: String,
-    val title: String,
-    val updatedAt: String,
-    val url: String
+    val title: String
 )
